099b5bb2bdbf378b2b20845bd23156f5576e25b4
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-11 14:53:21 +0430

    the email_login function was added to the accounts apps views.py file

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 import random
 from .models import VerificationCode
-from .forms import RegisterForm, UsernameLoginForm
+from .forms import RegisterForm, UsernameLoginForm, EmailLoginForm
 from django.utils import timezone
 from datetime import timedelta
 from django.core.mail import send_mail
 from django.contrib import messages
 from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 
 def generate_verification_code():
@@ -71,3 +72,41 @@ def username_login(request):
         'form': form
     }
     return render(request, 'accounts/username_login.html', context)
+
+
+def email_login(request):
+    if request.user.is_authenticated:
+        return redirect('core:home')
+
+    if request.method == 'POST':
+        form = EmailLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            try:
+                user = User.objects.get(email=email)
+                code = generate_verification_code()
+                VerificationCode.objects.create(
+                    user=user,
+                    code=code,
+                    code_expiry=timezone.now() + timedelta(minutes=2)
+                )
+                send_mail(
+                    'کد تایید ورود',
+                    f'خوش آمدید! برای ورود به حساب کاربری خود از کد زیر استفاده کنید.\n\nکد تایید شما: {code}\nلطفاً این کد را در صفحه تایید وارد کنید.',
+                    settings.EMAIL_HOST_USER,
+                    [email],
+                    fail_silently=False,
+                )
+                request.session['verification_type'] = 'login'
+                request.session['verified_user_id'] = user.id
+                messages.success(request, 'کد تایید به ایمیل شما ارسال شد.')
+                return redirect('accounts:verify_code')
+            except User.DoesNotExist:
+                messages.error(request, 'ایمیل وارد شده وجود ندارد.')
+    else:
+        form = EmailLoginForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/email_login.html', context)
